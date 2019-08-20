a62d759ce69c0af7e37d672dacac6a51f003c8d0
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-08-21 00:27:31 +0400

    Handling errors with fetch

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 // First way to fetch data from external api asynchronisly using async/await
-const getResource = async(url) => {
+const getResource = async (url) => {
     const res = await fetch(url);
+
+    if (!res.o) {
+        throw new Error(`Could not fetch ${url}, recieved ${res.status}`)
+    }
+
     const body = await res.json();
     return body;
 }
 
-getResource('https://swapi.co/api/people/1/')
+
+getResource('https:/as/swapi.co/api/people/12134321413541345')
     .then((body) => {
         console.log(body);
-    });
-
-
-// Second way (not so fast) using only .then
-fetch('https://swapi.co/api/people/1/')
-    .then((res) => {
-        return res.json();
     })
-    .then((body) => {
-        console.log(body);
+    .catch((err) => {
+        console.log('Could not fetch', err)
     });
+
+
 

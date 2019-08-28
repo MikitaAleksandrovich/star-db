71de56ab36836c355fdde1bb04fb4e09edecdff6
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-08-28 22:04:16 +0400

    Get each Person from Api and render them on items list

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,30 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 import './item-list.css';
+import SwapiService from '../../service/swapi-service';
+import Spinner from '../spinner';
+
 
 export default class ItemList extends Component {
+
+  SwapiService = new SwapiService();
+
+  state = {
+    peopleList: null
+  };
+
+  componentDidMount() {
+    this.SwapiService.getAllPeople().then((peopleList) => {
+      this.setState({
+        peopleList
+      });
+    });
+  } 
+
+  renderItems(arr) {
+    return arr.map(({id, name}) => {
+      return (
+        <li className="list-group-item"
+            key={id}>
+          {name}
+      </li>
+      );
+    });
+  }
+
     render() {
-        return (
-          <ul className="item-list list-group">
-            <li className="list-group-item">
-              Luke Skywalker
-            </li>
-            <li className="list-group-item">
-              Darth Vader
-            </li>
-            <li className="list-group-item">
-              R2-D2
-            </li>
-            <li className="list-group-item">
-              Leila Ordana
-            </li>
-            <li className="list-group-item">
-              Volha
-            </li>
-            <li className="list-group-item">
-              Nikita
-            </li>
-          </ul>
-        );
+
+      const { peopleList } = this.state;
+
+      if(!peopleList) {
+        return <Spinner/>
+      }
+
+      const items = this.renderItems(peopleList);
+
+      return (
+        <ul className="item-list list-group">
+          {items}
+        </ul>
+      );
     }
 };
--- src/service/swapi-service.js
+++ src/service/swapi-service.js
@@ -42,7 +42,7 @@ export default class SwapiService {
         return item.url.match(idRegExp)[1];
     }
 
-    _transformPlanet(planet) {
+    _transformPlanet = (planet) => {
         return {
             id: this._extractId(planet),
             name: planet.name,
@@ -52,7 +52,7 @@ export default class SwapiService {
         }
     }
 
-    _transformPerson(person) {
+    _transformPerson = (person) => {
         return {
             id: this._extractId(person),
             name: person.name,
@@ -62,7 +62,7 @@ export default class SwapiService {
         }
     }
 
-    _transformStarship(starship) {
+    _transformStarship = (starship) => {
         return {
             id: this._extractId(starship),
             name: starship.name,

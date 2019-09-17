83c89edd4252f06f468ccaabb56dd98488997904
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-09-17 20:44:36 +0400

    Create separate component for Error boundry

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,29 +1,10 @@
 import React, { Component } from 'react';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
-import ErrorIndicator from '../error-indicator';
 import SwapiService from '../../service/swapi-service';
 import Row from '../row';
+import ErrorBoundry from '../error-boundry';
 
-class ErrorBoundry extends Component {
-
-    state = {
-        hasError: false,
-    }
-
-    componentDidCatch() {
-        this.setState({
-            hasError: true,
-        })
-    }
-
-    render() {
-        if(this.state.hasError) {
-            return <ErrorIndicator/>
-        }
-        return this.props.children;
-    }
-}
 
 export default class PeoplePage extends Component {
 

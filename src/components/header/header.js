3ae37ec0f6fdc8f8389f01712a28236c871f26f0
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-10-29 20:05:52 +0300

    Use Link Component instead of <a> to go to page with reloading the App

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './header.css';
 
@@ -10,10 +11,10 @@ const Header = ({ onServiceChange }) => {
             </h3>
             <ul className="d-flex">
                 <li>
-                    <a href="people">People</a>
+                    <Link to="/people">People</Link>
                 </li>
                 <li>
-                    <a href="planets">Planets</a>
+                    <Link to="/people">People</Link>
                 </li>
                 <li>
                     <a href="starships">Starships</a>
